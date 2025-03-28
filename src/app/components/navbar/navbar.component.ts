b73534494240180136f6bee85982e2377c62bba4
uness1 <yelhor2@gmail.com>
2025-03-28 17:28:05 +0000

    change

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,11 @@
 
 
+import { CommonModule } from '@angular/common';
 import { Component, OnInit, signal } from '@angular/core';
-import { RouterLink } from '@angular/router';
-// import { SelectModule } from 'primeng/select';
+import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterModule } from '@angular/router';
+import { SelectModule } from 'primeng/select';
+import { DropdownModule } from 'primeng/dropdown';
 
 
 
@@ -10,6 +13,10 @@ interface sub_menu {
   name: string;
   path?: string | Function;
   isFunction?: boolean;
+}
+interface ICity{
+  name: string;
+  code: string;
 }
 
 interface menu_item {
@@ -25,11 +32,19 @@ interface IsubNavItems{
 }
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+    RouterLink,
+    FormsModule,
+    SelectModule,
+    DropdownModule
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent{
+export class NavbarComponent implements OnInit {
 // menu: menu_item[] = [
 // {
 //   name: 'Home',
@@ -131,10 +146,29 @@ subNavItems = signal<{id:number, name: string, path:string}[]>([]);
 //   {id: 7, name: 'Section 2', path:'Helmets'},
 //   {id: 8, name: 'Section 3', path:'Helmets'}
 // ];
+// cities : ICity [] = [
+//   { name: 'Riyadh', code: 'RYD' },
+//   { name: 'Jeddah', code: 'JED' },
+//   { name: 'Dammam', code: 'DAM' }
+// ];
+
+// selectedCity: any;
+userName: string = 'Ahmed';
+cities: ICity[] | undefined;
+
+    selectedCity: ICity | undefined;
+
+    ngOnInit() {
+        this.cities = [
+          { name: 'Riyadh', code: 'RYD' },
+          { name: 'Jeddah', code: 'JED' },
+          { name: 'Dammam', code: 'DAM' }
+        ];
+    }
 
 changeSection(section: string) {
   if (section === 'home') {
-    this.subNavItems.set([{id:1,name:'accessories', path:'plates'},{ id:2,name:'Riding Gear', path:'Helmets'}, {id:3,name:'Accessories', path:'Helmets'}, {id:4,name:'Tires', path:'Helmets'}, {id:5,name:'Aftermarket', path:'Helmets'}, {id:6,name:'Section 1', path:'Helmets'}, {id:7,name:'Section 2', path:'Helmets'}, {id:8,name:'Section 3', path:'Helmets'}]);
+    this.subNavItems.set([{id:1,name:'Helmets', path:'Helmets'},{ id:2,name:'Riding Gear', path:'RidingGear'}, {id:3,name:'Accessories', path:'accessories'}, {id:4,name:'Tires', path:'Tires'}, {id:5,name:'Aftermarket', path:'Aftermarket'}, {id:6,name:'Section 1', path:'Section'}, {id:7,name:'Section 2', path:'Section'}, {id:8,name:'Section 3', path:'Section'}]);
   } else if (section === 'usedParts') {
     this.subNavItems.set([{id:1,name:'Motorcycles', path:'Helmets'}, {id:2,name:'Spare Parts', path:'Helmets'}, {id:3,name:'Plates', path:'plates'}]);
   } else {
