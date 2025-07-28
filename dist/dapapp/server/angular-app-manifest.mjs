1a66b3250ba45fb70c3f94764216ae85229b95f4
younesselhor <yelhor7@gmail.com>
2025-07-28 21:04:34 +0100

    update

--- dist/dapapp/server/angular-app-manifest.mjs
+++ dist/dapapp/server/angular-app-manifest.mjs
@@ -8,14 +8,14 @@ export default {
   entryPointToBrowserMapping: {
   "src/main.ts": [
     {
-      "path": "main-ORNB5BC5.js",
+      "path": "main-ETXHRRBF.js",
       "dynamicImport": false
     }
   ]
 },
   assets: {
-    'index.csr.html': {size: 18228, hash: 'c5a0c391a8718e5a7d734f2daf011079c10070d3edda59ec4bab277ad8d8cf58', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 9000, hash: 'dbc38ded2b9096bcddb1a5a880ab769ba47dda02b27e152aa2453b9887b39e59', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'styles-EP5AT5P7.css': {size: 128904, hash: 'p3JdZhEE3rY', text: () => import('./assets-chunks/styles-EP5AT5P7_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 20330, hash: 'cff0d1da5ba3523158d8abc603a224d206ccba6dbbf613f7f6db6671e1e8efc6', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 9860, hash: '7f50271c034cb62b35ce70ed0be542759a1d44ecddbcf0110c965188cfdaf5bc', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'styles-YYH3UU5U.css': {size: 135151, hash: 'mQxxI4Dfphk', text: () => import('./assets-chunks/styles-YYH3UU5U_css.mjs').then(m => m.default)}
   },
 };
